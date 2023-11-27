c8448c7c78622d3570057a98ce70016b78e03ed4
Eider Muñoz <eider.munoz@asmetsalud.com>
2023-11-27 01:03:01 -0500

    reorganizacion de app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {path: '', redirectTo: '/login/iniciar', pathMatch: 'full'},
+  {path: '**', redirectTo: '/login/iniciar', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './app/componentes/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SesionService } from "./app/service/sesion.service";
+import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
+import { PeticionInterceptor } from "./app/service/peticion.interceptor";
+import { LocalService } from "./app/service/local.service";
+import { StorageService } from "./app/service/storage.service";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatMenuModule } from "@angular/material/menu";
 
 @NgModule({
   declarations: [
@@ -14,9 +23,19 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgbModule
+    HttpClientModule,
+    NgbModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
   ],
-  providers: [],
+  providers: [
+    SesionService,
+    LocalService,
+    StorageService,
+    { provide: HTTP_INTERCEPTORS, useClass: PeticionInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
